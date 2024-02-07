b7519e17e5d3e2edf729e3e33e35b59fc846c71f
Samir <samir.gasimov.00@gmail.com>
2024-02-07 18:20:34 +0400

    Prepare layout components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './components/Home'
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h2>App component</h2>
+      <Header/>
+      <Home/>
+      <Footer/>
     </div>
   );
 }
