95bea27cd68d6fb39667923e42faf15deb255b3e
Samir <samir.gasimov.00@gmail.com>
2024-02-08 15:20:20 +0400

    new node_modules

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './components/Home'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        <Header />
+        <Home />
+        <Footer />
+      
+    
+    </div>
+  );
+}
+
+export default App;
