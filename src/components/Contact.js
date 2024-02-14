62f7755947e65cfca9ddcacd2d39bbe2fba239b3
Samir <samir.gasimov.00@gmail.com>
2024-02-14 16:51:59 +0400

    Contact page html css create

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import contact_image from '../image/contact.webp'
 
 export default function Contact() {
   return (
-    <div>
-          <h2>Contact page</h2>
-    </div>
+    <section id='Contact'>
+      <div className="container">
+        <div className="contact-main">
+          <div className="contact-image">
+            <img src={contact_image} alt="contact" />
+          </div>
+          <div className="contact-form-div">
+            <form action="" className='contact-form'>
+              <input type="text" placeholder='Name and Surname'/>
+              <input type="mail" placeholder='Email Address'/>
+              <input type="text" placeholder='Phone number (Optional)' />
+              <textarea name="" id="" rows="11" placeholder='Message'></textarea>
+              <button type='submit'>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
