3b51d6cc4ba659c81c172a5d0cbbcd64fe3cc597
Samir <samir.gasimov.00@gmail.com>
2024-02-20 19:50:06 +0400

    register page html css created

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import register_image from '../image/register.webp'
 
 export default function Register() {
   return (
-    <div>
-      <h2>Register</h2>
-    </div>
+    <section id='Register'>
+      <div className="container">
+        <div className="register-main">
+          <div className="register-form-div">
+            <form action="" className='register-form'>
+              <input type="text" placeholder='User name' />
+              <input type="mail" placeholder='Email' />
+              <input type="password" placeholder='Password' />
+              <input type="password" placeholder='Password Again' />
+              <button>Register</button>
+            </form>
+            <Link to="/login" className="register-login-link">Have an account ?<span>Login</span></Link>
+          </div>
+          <div className="register-image">
+            <img src={register_image} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
